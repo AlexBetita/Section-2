10d50f1ade640907b0b362208419bd40cf8b16bc
AlexBetita <alexbheb25@gmail.com>
2023-10-14 09:21:43 -0700

    lecture

---
+++ 4-Module/5-week/4-day/models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+	  username: {
+		  type: DataTypes.STRING,
+		  validate: {
+			  len: [2, 10]
+		  }
+	  },
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
